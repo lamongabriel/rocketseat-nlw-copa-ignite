fe496622a071f4b009ddf2477e8d42a3ee358f2a
Gabriel Lamon <gabriel-lamon@outlook.com>
2022-11-03 17:22:41 -0300

    react native mobile components for whole application

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,7 @@
 import { NativeBaseProvider, StatusBar } from 'native-base'
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
-import { SignIn } from './src/screens/SignIn'
+import { New } from './src/screens/New'
 import { Loading } from './src/components/Loading'
 
 import { THEME } from './src/styles/theme'
@@ -14,7 +14,7 @@ export default function App () {
     <NativeBaseProvider theme={THEME}>
         <AuthContextProvider>
           <StatusBar barStyle='light-content' backgroundColor="transparent" translucent/>
-          { fontsLoaded ? <SignIn /> : <Loading/> }
+          { fontsLoaded ? <New /> : <Loading/> }
         </AuthContextProvider>
     </NativeBaseProvider>
   )
--- mobile/src/components/Button.tsx
+++ mobile/src/components/Button.tsx
@@ -1,27 +1,34 @@
-import { Button as NativeBaseButton, Text, IButtonProps } from 'native-base'
+import { Button as ButtonNativeBase, IButtonProps, Text } from 'native-base'
 
-interface ButtonProps extends IButtonProps {
+interface Props extends IButtonProps {
   title: string
-  type?: 'RED' | 'YELLOW'
+  type?: 'PRIMARY' | 'SECONDARY'
 }
 
-export function Button ({ title, type = 'YELLOW', ...rest }: ButtonProps) {
+export function Button ({ title, type = 'PRIMARY', ...rest }: Props) {
   return (
-    <NativeBaseButton
-      w='full'
+    <ButtonNativeBase
+      w="full"
       h={14}
-      rounded='sm'
-      fontSize='md'
-      textTransform='uppercase'
-      bg={type === 'RED' ? 'red.500' : 'yellow.500'}
-      _pressed={{ bg: type === 'RED' ? 'red.600' : 'yellow.600' }}
+      rounded="sm"
+      fontSize="md"
+      textTransform="uppercase"
+      bg={type === 'SECONDARY' ? 'red.500' : 'yellow.500'}
+      _pressed={{
+        bg: type === 'SECONDARY' ? 'red.400' : 'yellow.600'
+      }}
+      _loading={{
+        _spinner: { color: 'black' }
+      }}
       {...rest}
     >
       <Text
-        fontSize='sm'
-        fontFamily='heading'
-        color={type === 'RED' ? 'white' : 'black'}
-      >{title}</Text>
-    </NativeBaseButton>
+        fontSize="sm"
+        fontFamily="heading"
+        color={type === 'SECONDARY' ? 'white' : 'black'}
+      >
+        {title}
+      </Text>
+    </ButtonNativeBase >
   )
 }
